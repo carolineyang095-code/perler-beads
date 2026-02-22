3e443ecd62066409f4baeaa3b776fca2da4dbe2d
carolineyang095-code <carolineyang095@gmail.com>
2026-02-22 22:47:04 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("perler-beads-six.vercel.app/focus"); // <- 改成你的工作台路径，比如 /workspace /editor 等
+  redirect("perler-beads-six.vercel.app"); // <- 改成你的工作台路径，比如 /workspace /editor 等
 }
